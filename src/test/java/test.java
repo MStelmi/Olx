fcffb03a1dd4cfd0de53cb8c3a507021c3070712
mstelmi <michala.stelmach@gmail.com>
2019-02-19 15:06:47 +0100

    Added annotations BeforeEach and AfterEach

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,18 +1,35 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test {
-    @Test
-    public void runGooglePage(){
-        WebDriver driver;
+
+    WebDriver driver;
+    String url = "https://www.google.com";
+
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+    }
 
-        driver.get("https://www.google.com");
+    @BeforeEach
+    void openSession() {
+        setup();
+        driver.get(url);
+    }
 
+    @AfterEach
+    void closeSession() {
+        driver.quit();
+        System.out.println("session is closed");
+    }
+
+    @Test
+    public void runGooglePage() {
         driver.findElement(By.name("q")).sendKeys("wikipedia");
         driver.findElement(By.name("btnK")).click();
 
