0ecb7e56c5700bf524eb12c8cc1d851e5f17347e
mstelmi <michala.stelmach@gmail.com>
2019-02-28 11:07:04 +0100

    fixes for abstractTest and SearchCarTest classes

--- src/test/java/pl/olx/tests/AbstractTest.java
+++ src/test/java/pl/olx/tests/AbstractTest.java
@@ -3,18 +3,17 @@ package pl.olx.tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pl.olx.pages.HomePage;
 import pl.olx.pages.ResultPage;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AbstractTest {
+    String url = "https://www.olx.pl/";
 
     WebDriver driver;
-    HomePage homePage;
-    ResultPage resultPage;
+    public HomePage homePage;
+    public ResultPage resultPage;
 
     public void setup() {
         WebDriverManager.chromedriver().setup();
@@ -22,35 +21,15 @@ public class AbstractTest {
         homePage = new HomePage(driver);
         resultPage = new ResultPage(driver);
     }
-
-    boolean results;
-    String url = "https://www.olx.pl/";
-    String brandCar = "honda";
-    String modelCar = "accord";
-
-
     @BeforeEach
     void openSession() {
         setup();
         driver.get(url);
         driver.manage().window().fullscreen();
     }
-
     @AfterEach
     void closeSession() {
         driver.quit();
         System.out.println("session is closed");
-    }
-
-    @Test
-    public void searchCarTest() throws InterruptedException {
-        homePage.clickMotoryzacja();
-        homePage.clickSamochodyOsobowe();
-        homePage.clickMarka();
-        homePage.selectBrandCar(brandCar);
-        homePage.clickModel();
-        homePage.selectModelCar(modelCar);
-        results = (resultPage.verifyResults(brandCar + " " + modelCar));
-        assertTrue(results);
     }
 }
--- src/test/java/pl/olx/tests/SearchCarTest.java
+++ src/test/java/pl/olx/tests/SearchCarTest.java
@@ -1,57 +1,21 @@
 package pl.olx.tests;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SearchCarTest {
-
-    WebDriver driver;
-    String url = "https://www.olx.pl";
-
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-    }
-
-    @BeforeEach
-    void openSession() {
-        setup();
-        driver.get(url);
-        driver.manage().window().fullscreen();
-    }
-
-    @AfterEach
-    void closeSession() {
-        driver.quit();
-        System.out.println("session is closed");
-    }
+public class SearchCarTest extends AbstractTest{
+    String brandCar = "honda";
+    String modelCar = "accord";
+    boolean results;
 
     @Test
-    public void findCar() throws InterruptedException {
-        String results;
-        String brandCar = "honda";
-        String modelCar = "accord";
-
-        driver.findElement(By.linkText("Samochody osobowe")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//span[@data-default-label='Marka']")).click();
-        driver.findElement(By.xpath("//a[@data-code='" + brandCar + "']")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//li//div//a//span[@data-default-label='Model']")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//input[@type='checkbox' and @data-value='" + modelCar + "']")).click();
-        Thread.sleep(10000);
-        results = driver.findElement(By.xpath("//table[@summary='Ogłoszenia']")).getText().toLowerCase();
-        boolean x = results.contains(brandCar + " " + modelCar);
-        assertTrue(x);
-
-        System.out.println(results);
-        driver.close();
+    public void SearchCarTest() throws InterruptedException {
+        homePage.clickMotoryzacja();
+        homePage.clickSamochodyOsobowe();
+        homePage.clickMarka();
+        homePage.selectBrandCar(brandCar);
+        homePage.clickModel();
+        homePage.selectModelCar(modelCar);
+        results = (resultPage.verifyResults(brandCar + " " + modelCar));
+        assertTrue(results);
     }
 }
